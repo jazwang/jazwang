96a23385cbb9af50e3cbba3b0dbbef447c2384c3
Jasmine Wang <jwang131@u.rochester.edu>
2020-12-05 22:57:04 -0800

    initial commit

---
+++ src/components/BackgroundTheme.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ThemePage from '../images/jz.png';
+
+const BackgroundTheme = (props) => {
+    
+    return (
+        <img src={ThemePage} />
+    );
+};
+
+export default BackgroundTheme;
+
+// const BackgroundImg = styled.img`
+//     height: 60
