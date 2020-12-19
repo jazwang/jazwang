00197028cc16a976ada0fbbcca46f48f30ee3ca2
Jasmine Wang <jwang131@u.rochester.edu>
2020-12-19 14:45:02 -0800

    add div to contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,14 +2,10 @@ import React from 'react';
 import '../../App.css';
 
 export default function Contact() {
-  return <h1 className='contact'>contact</h1>;
-  // return (
-  //   <div className='contact'>
-  //     <h1>contact</h1>
-  //     <p>
-        
-  //     </p>
-  //   </div>
-  
-  // );
+  return (
+  <div className="page">
+    <h1>keep in touch</h1>
+    
+  </div>
+  )
 }
