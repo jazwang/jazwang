7e729f67f60cab640771891810bab68fd56d6827
Jasmine Wang <jwang131@u.rochester.edu>
2020-12-19 23:13:24 -0800

    added underline to video link

--- src/components/pages/Video.js
+++ src/components/pages/Video.js
@@ -6,7 +6,7 @@ export default function Video() {
     <div className="page">
     <h1>video</h1>
     <a href="https://www.youtube.com/channel/UC3SSXBY7FpecK0d9r7n9uqA"
-      rel="noreferrer" target="_blank" style={{display: 'flex', fontSize: '25px'}}>Coding With Jaz</a>
+      rel="noreferrer" target="_blank" style={{display: 'flex', fontSize: '25px', textDecoration: 'underline'}}>Coding With Jaz</a>
     </div>
   )
 }
