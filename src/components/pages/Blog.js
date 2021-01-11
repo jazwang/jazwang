7fac719a1ee4932a41fa1422e8ee944e6fdb1b2c
Jasmine Wang <jwang131@u.rochester.edu>
2021-01-10 17:50:07 -0800

    blog grab info

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import '../../App.css';
+import React, { useState, useEffect } from 'react'
+
+const Cosmic = require('cosmicjs')
+const api = Cosmic()
+// Set these values, found in Bucket > Settings after logging in at https://app.cosmicjs.com/login
+const bucket = api.bucket({
+  slug: 'jazwang',
+  read_key: 'Bpsj6iYWZw6MI9fjQCsgFtaXHvqBDP6bjuf12EMFh5CxaAZb2R'
+})
 
 export default function Blog() {
+
+  // Use Hooks to get page data from Cosmic!
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchBlog = async () => {
+      const data = await bucket.getObjects({
+        type: 'blogs',
+        props: 'slug,title,content' // Limit the API response data by props
+      })
+      setData(data)
+    };
+    fetchBlog()
+  }, []);
+  if (!data)
+    return <div className="page">
+    <h1>blog</h1>
+    </div>
+  const posts = data.objects
+
   return (
     <div className="page">
     <h1>blog</h1>
-    <h1>
-      Coming soon...
-    </h1>
+    { posts.map(post => 
+      <div key={post.slug} style={{marginBottom: 20}}>
+        <div>{post.title}</div>
+      </div>)
+    }
     </div>
   )
 }
